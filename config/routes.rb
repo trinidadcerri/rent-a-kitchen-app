7a89bb6d5678aaefa5dbcdeb98d4443b02205d15
trinidad cerri <trinidad.cerri@gmail.com>
2023-03-07 12:18:47 +0100

    routes without typo errors now

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: "kitchens#index"
   resources :users, except: [:index] do
-    resources :kitchens,
-    resources :bookings, only: [:new :create :show]
+    resources :kitchens
+    resources :bookings, only: [:new, :create, :show]
     resources :reviews, only: [:create]
   end
-  resources :kitchens, only: :edit :destroy do
+  resources :kitchens, only: [:edit, :destroy] do
     resources :reviews, only: [:create]
   end
   resources :bookings, only: :destroy
