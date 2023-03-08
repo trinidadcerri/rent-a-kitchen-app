570ad9810b51a7f2f2a297d49c1c5ebab917e78a
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-08 16:16:57 +0200

    WOrk in progress on my home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+10.times do
+  name = Faker::Name.unique.name
+  address = Faker::Address.street_name
+  description = Faker::Name.name
+  capacity = rand(1..3)
+  price = [75, 100, 125, 150, 200].sample
+  photo_url = 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+  Kitchen.create(name: name, address: address, description: description, capacity: capacity, price: price, photo_url: photo_url)
+  puts "kitchens created"
+end
