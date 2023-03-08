c2946cfe7ea8b03bc9897e3ceff4dbfa9632340c
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-08 13:26:12 +0200

    New/create actions added

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,5 +1,24 @@
 class KitchensController < ApplicationController
+
   def index
     @kitchens = Kitchen.all
+  end
+
+  def new
+    @kitchen = Kitchen.new
+  end
+
+  def create
+    @kitchen = Kitchen.new(kitchen_params)
+    if @kitchen.save
+      redirect_to kitchens_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+  def kitchen_params
+    params.require(:kitchen).permit(:name, :address, :description, :price, :capacity, :photo_url)
   end
 end
