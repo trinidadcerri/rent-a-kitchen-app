2070ffb1eb76c40894d02d6225db09debc1472f5
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-15 16:35:59 +0200

    Working on reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_103739) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_143338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_103739) do
     t.index ["user_id"], name: "index_kitchens_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "raiting"
+    t.bigint "user_id", null: false
+    t.bigint "kitchen_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["kitchen_id"], name: "index_reviews_on_kitchen_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -89,4 +100,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_103739) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "kitchens"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "kitchens"
+  add_foreign_key "reviews", "users"
 end
