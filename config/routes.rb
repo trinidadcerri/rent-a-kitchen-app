b61d27632e0da6155439bccb8e0b28e2cbd1977f
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-15 16:19:14 +0200

    Missing required parameter: client_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   root to: "kitchens#index"
   resources :kitchens do
     resources :bookings, only: [:new, :create, :show]
